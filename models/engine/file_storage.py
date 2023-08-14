983f8933f63027e38cd534e35197155b483cc85d
drhydeandmrjekyll <ronaldkevinnene@gmail.com>
2023-08-13 17:20:40 -0700

    edit

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -6,6 +6,15 @@ to a JSON file and deserializes JSON files to instances.
 import json
 import os
 from base_model import BaseModel
+import os
+import sys
+
+# Get directory of the current script
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Append parent directory to sys.path
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
 
 
 class FileStorage:
